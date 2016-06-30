e638a5d06bcbc783ea85941bba6875084a2465dd
Supriya2807 <lahadesupriya@gmail.com>
2016-06-30 11:00:25 +0530

    session1assign1
    session1assign1

---
+++ Session1Assign1/app/src/main/java/com/supriyalahade/session1assign1/FirstAssignmentActivity.java
@@ -0,0 +1,21 @@
+package com.supriyalahade.session1assign1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FirstAssignmentActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_assignment);
+
+
+    }
+
+
+}
